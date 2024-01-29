1da393a8da34014b998ed805c2005ab71955471f
MirFaizan01 <faizanmeer322@gmail.com>
2024-01-30 00:21:54 +0530

    added the fornt page html css

---
+++ script.js
@@ -0,0 +1,29 @@
+function loading(){
+    var tl = gsap.timeline();
+tl.to(".yellow1",{
+    top:"-100%",
+    delay:.5,
+    duartion:.5,
+    ease:expo.out
+})
+tl.to(".yellow2",{
+    top:"100%",
+    delay:.5,
+    duartion:.5,
+    ease:expo.out
+},"anime")
+tl.to(".loader h1",{
+    delay:.5,
+    duartion:.5,
+    color:"black"
+},"anime")
+tl.to(".loader",{
+    display:"none"
+})
+}
+loading();
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});

afc244735fc8e3aa710c69f408f08a4acb9e9a58
MirFaizan01 <faizanmeer322@gmail.com>
2024-01-30 23:57:30 +0530

    completed the loader

--- script.js
+++ script.js
@@ -4,36 +4,33 @@
     smooth: true
 }); */
 var tl = gsap.timeline();
-tl.to(".yellow1",{
-    top:"-100%",
-    delay:0.5,
-    duartion:0.5,
-    ease:expo.out
-})
+tl.to(".yellow1", {
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.5,
+    ease: "expo.out"
+});
 
+tl.from(".yellow2", {
+    top: "100%",
+    delay: 0.7,
+    duration: 0.5,
+    ease: "expo.out"
+}, "anime");
 
-tl.from(".yellow2",{
-    top:"100%",
-    delay:0.7,
-    duartion:0.5,
-    ease:expo.out
-},"anime")
+tl.to(".loader h1", {
+    delay: 0.6,
+    duration: 0.5,
+    color: "black"
+}, "anime");
 
+tl.to(".loader", {
+    opacity: 0
+});
 
-tl.to(".loader h1",{
-    delay:0.6,
-    duartion:0.5,
-    color:"black"
-},"anime")
-
-
-tl.to(".loader",{
-    opacity:0
-})
-
-tl.to(".loader",{
-    display:"none"
-})
+tl.to(".loader", {
+    display: "none"
+});
 
 
 
